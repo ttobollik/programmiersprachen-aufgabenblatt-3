c8391c5a9435ab20b89f136074eda95c95bc9412
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-05-23 17:36:07 +0200

    method for finding numbers divisable by three

--- source/aufgabe_11.cpp
+++ source/aufgabe_11.cpp
@@ -2,12 +2,27 @@
 #include <catch.hpp>
 #include <cmath>
 #include <algorithm>
-TEST_CASE("filter alle vielfache von drei", "[erase]") {
-// ihre Loesung : // ...
-REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3)); }
+#include <vector>
+// TEST_CASE("filter alle vielfache von drei", "[erase]") {
+// // ihre Loesung : // ...
+// REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3)); }
+
+bool is_multiple_of_3(int i){
+    return (i % 3) != 0;
+}
 
 int main(int argc, char* argv[]) {
 
+std::vector<unsigned int> no_three(100);
+for(auto& i : no_three) {
+    i = rand() % 100;
+}
+
+no_three.erase(std::remove_if( no_three.begin(), no_three.end(), is_multiple_of_3), no_three.end() ); 
+
+for(auto& i : no_three) {
+    std::cout << i << ",";
+}
 
 return Catch::Session().run(argc, argv);
 
