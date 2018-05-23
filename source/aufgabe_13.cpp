775a05217379ea1398381731215f73a8d549f1a5
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-05-23 19:59:12 +0200

    template to filter objects

--- source/aufgabe_13.cpp
+++ source/aufgabe_13.cpp
@@ -1,3 +1,46 @@
-int main() {
-    return 0;
+#define CATCH_CONFIG_RUNNER
+#include <catch.hpp>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <cstdlib>
+#include <iterator>
+
+
+template <typename T, typename F>
+T filter(T const& obj1, F booleanF) {
+   T result; 
+   //std::copy_if(obj1.begin(), obj1.end(), result.begin(), booleanF); ADRIAN FRAGEN-- warum?
+   for(auto& i : obj1) {
+       if (booleanF(i) == true) {
+           result.push_back(i);
+       }
+   }
+   return result;
 }
+
+bool is_even(int n) {
+    return n % 2 == 0;
+    }
+    
+std::vector<int> v{1,2,3,4,5,6};
+std::vector<int> all_even = filter(v, is_even);
+
+TEST_CASE("filter alle geraden Zahlen", "[even]") {
+
+REQUIRE(std::all_of(all_even.begin(), all_even.end(), is_even));
+}
+
+
+int main(int argc, char* argv[]) {
+     for(auto& i : all_even) {
+        std::cout << i << ", ";
+    }
+    
+    return Catch::Session().run(argc, argv);
+
+   
+
+}
+
+
