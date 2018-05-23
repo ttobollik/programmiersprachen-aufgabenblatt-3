683a60a8e5bde9240dd8cd44198b3aecd4c352f7
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-05-23 17:52:41 +0200

    tests for removing numbers not divisable by three

--- source/aufgabe_11.cpp
+++ source/aufgabe_11.cpp
@@ -3,13 +3,23 @@
 #include <cmath>
 #include <algorithm>
 #include <vector>
-// TEST_CASE("filter alle vielfache von drei", "[erase]") {
-// // ihre Loesung : // ...
-// REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3)); }
+
+
+std::vector<unsigned int> all_three(100);
 
 bool is_multiple_of_3(int i){
+    return (i % 3) == 0;
+}
+
+bool is_not_multiple_of_3(int i){
     return (i % 3) != 0;
 }
+
+TEST_CASE("filter alle vielfache von drei", "[erase]") {
+
+REQUIRE(std::all_of(all_three.begin(), all_three.end(), is_multiple_of_3)); }
+
+
 
 int main(int argc, char* argv[]) {
 
@@ -18,12 +28,18 @@ for(auto& i : no_three) {
     i = rand() % 100;
 }
 
-no_three.erase(std::remove_if( no_three.begin(), no_three.end(), is_multiple_of_3), no_three.end() ); 
+no_three.erase(std::remove_if( no_three.begin(), no_three.end(), is_not_multiple_of_3), no_three.end() ); 
 
 for(auto& i : no_three) {
     std::cout << i << ",";
 }
 
+//
+for(auto& i : all_three) {
+    i = rand() % 100;
+}
+all_three.erase(std::remove_if( all_three.begin(), all_three.end(), is_not_multiple_of_3), all_three.end() );
+ 
 return Catch::Session().run(argc, argv);
 
 }
